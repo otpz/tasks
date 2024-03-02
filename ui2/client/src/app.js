1ff734c15f450851035ce14d0a8d4fa2c18febb5
Osman Topuz <osmantopuz98@gmail.com>
2024-03-02 21:18:17 +0300

    try-catch blocks added

--- ui2/client/src/app.js
+++ ui2/client/src/app.js
@@ -1,10 +1,30 @@
 const formSubmit = (e) => {
     e.preventDefault()
     if (e.target.city.value) {
-        const englishCity = turkishToEnglishLetters(e.target.city.value)
-        console.log(englishCity)
-        fetchData(englishCity)
+        const cityEng = turkishToEnglishLetters(e.target.city.value)
+        fetchData(cityEng)
     }
+}
+
+const fetchData = async (city) => {
+    try {
+        const response = await fetch(`http://localhost:5000/${city}`, {method: 'GET'})
+        const data = await response.json()
+        setValuesToDom(data)
+    } catch (error) {
+        console.error(error)
+        return error
+    }
+}
+
+const setValuesToDom = (data) => {
+    const spanVmaxDiesel = document.querySelector('#maxdiesel')
+    const spanVproDiesel = document.querySelector('#prodiesel')
+    const spanPoGas = document.querySelector('#pogas')
+
+    spanVmaxDiesel.innerHTML = `$${data.VMaxDiesel}`
+    spanVproDiesel.innerHTML = `$${data.VProDiesel}`
+    spanPoGas.innerHTML = `$${data.POGas}`
 }
 
 const turkishToEnglishLetters = (text) => {
@@ -34,29 +54,4 @@ const turkishToEnglishLetters = (text) => {
     return text.replace(/[ğĞıİçÇşŞüÜöÖ]/g, function(match) {
         return turkishLetters[match];
     })
-}
-
-const fetchData = async (city) => {
-
-    let headers = new Headers()
-
-    headers.append('Content-Type', 'application/json')
-    headers.append('Accept', 'application/json')
-
-    const response = await fetch(`http://localhost:5000/${city}`, {method: 'GET'})
-    const data = await response.json()
-
-
-    setValuesToDom(data)
-}
-
-const setValuesToDom = (data) => {
-    console.log(data)
-    const spanVmaxDiesel = document.querySelector('#maxdiesel')
-    const spanVproDiesel = document.querySelector('#prodiesel')
-    const spanPoGas = document.querySelector('#pogas')
-
-    spanVmaxDiesel.innerHTML = `$${data.VMaxDiesel}`
-    spanVproDiesel.innerHTML = `$${data.VProDiesel}`
-    spanPoGas.innerHTML = `$${data.POGas}`
 }
--- ui2/server/index.js
+++ ui2/server/index.js
@@ -14,30 +14,28 @@ app.use(
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/:city', (req, res) => {
+app.get('/:city', async (req, res) => {
     const city = req.params // { city: 'usak' }
 
-    const cityUpperCase = city.city.toUpperCase(); // USAK
-    const cityLowerCase = city.city.toLowerCase(); // usak
+    const cityUpperCase = city.city.toUpperCase() // USAK
+    const cityLowerCase = city.city.toLowerCase() // usak
 
     const url = `https://www.petrolofisi.com.tr/akaryakit-fiyatlari/${cityLowerCase}-akaryakit-fiyatlari`
 
-    axios.get(url)
-        .then(response => {
-            
-            const $ = cheerio.load(response.data) // html içeriğini cheerio ile işle
+    try {
+        const data = await axios.get(url) // html içeriğini cheerio ile işle
+        const $ = cheerio.load(data.data)
 
-            // ilgili verileri al
-            const VMaxDiesel = $(`[data-disctrict-name="${cityUpperCase}"] > td:nth-child(3) > span:nth-child(1)`).text();
-            const VProDiesel = $(`[data-disctrict-name="${cityUpperCase}"] > td:nth-child(4) > span:nth-child(1)`).text();
-            const POGas = $(`[data-disctrict-name="${cityUpperCase}"] > td:nth-child(5) > span:nth-child(1)`).text();
-            return res.json({VMaxDiesel, VProDiesel, POGas})
-        })
-        .catch(error => {
-            return error
-        })
+        // ilgili verileri al
+        const VMaxDiesel = $(`[data-disctrict-name="${cityUpperCase}"] > td:nth-child(3) > span:nth-child(1)`).text()
+        const VProDiesel = $(`[data-disctrict-name="${cityUpperCase}"] > td:nth-child(4) > span:nth-child(1)`).text()
+        const POGas = $(`[data-disctrict-name="${cityUpperCase}"] > td:nth-child(5) > span:nth-child(1)`).text()
 
-    
+        return res.json({VMaxDiesel, VProDiesel, POGas})
+    }
+    catch (error){
+        return error
+    }
 })
 
 const port = 5000
